b2f55da12aa032a61714e447cc55513e9c6b9fd1
SonGeon <geons.dev@gmail.com>
2021-05-09 03:06:27 +0900

    Create Project

---
+++ src/main/java/com/apt/proptech/domain/Account.java
@@ -0,0 +1,40 @@
+package com.apt.proptech.domain;
+
+import com.apt.proptech.domain.enums.AccountState;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.sql.Date;
+
+/**
+ *
+ *  @author : SON
+ *  @since 2021. 05. 08
+ *  @version 1.0
+ *  @see  :
+ *  @revision :
+ *  @Description : 사용자가 저장한 은행계좌 정보 도메인
+ *
+ */
+
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Account {
+    Long ID;
+    String bankName;
+    String bankAccount;
+
+    AccountState state;
+
+    Date regDate;
+    Date updateDate;
+    Date lastUseDate;
+
+
+
+}
