1e9f8b4511652ef68659e9bf6c12a148743021aa
SonGeon <guns716@kpu.ac.kr>
2021-06-10 02:43:11 +0900

    시큐리티 로그인 처리/ h2 데이터베이스 더미데이터 설정

--- src/main/java/com/apt/proptech/config/SecurityConfig.java
+++ src/main/java/com/apt/proptech/config/SecurityConfig.java
@@ -27,7 +27,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter{
 			.antMatchers("/user/**").authenticated()
 			//.antMatchers("/admin/**").access("hasRole('ROLE_ADMIN') or hasRole('ROLE_USER')")
 			//.antMatchers("/admin/**").access("hasRole('ROLE_ADMIN') and hasRole('ROLE_USER')")
-			.antMatchers("/admin/**").access("hasRole('ROLE_ADMIN')")
+			.antMatchers("/dash/**").access("hasRole('ROLE_ADMIN')")
 			.anyRequest().permitAll()
 		.and()
 			.formLogin()
--- src/main/resources/sql/schema.sql
+++ src/main/resources/sql/schema.sql
@@ -4,8 +4,7 @@
 -- CREATE DATABASE IF NOT EXISTS `proptech`
 -- USE `proptech`;
 
-DROP TABLE IF EXISTS `account`;
-CREATE TABLE IF NOT EXISTS `account` (
+CREATE TABLE account (
   `id` bigint(20) NOT NULL AUTO_INCREMENT,
   `user_id` bigint(20) DEFAULT NULL,
   `description` varchar(50) DEFAULT NULL,
@@ -16,10 +15,9 @@ CREATE TABLE IF NOT EXISTS `account` (
   `reg_date` date DEFAULT NULL,
   `modi_date` date DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8;
+);
 
-DROP TABLE IF EXISTS `associate`;
-CREATE TABLE IF NOT EXISTS `associate` (
+CREATE TABLE `associate` (
   `id` bigint(20) NOT NULL AUTO_INCREMENT,
   `name` varchar(50) DEFAULT NULL,
   `associate_state` varchar(50) DEFAULT 'CREATE',
@@ -30,10 +28,10 @@ CREATE TABLE IF NOT EXISTS `associate` (
   `state` varchar(50) DEFAULT NULL,
   `address` varchar(50) DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
+);
 
-DROP TABLE IF EXISTS `claim`;
-CREATE TABLE IF NOT EXISTS `claim` (
+
+CREATE TABLE `claim` (
   `id` bigint(20) NOT NULL AUTO_INCREMENT,
   `sale_prop_id` bigint(20) DEFAULT NULL,
   `payment` bigint(20) DEFAULT NULL,
@@ -41,10 +39,10 @@ CREATE TABLE IF NOT EXISTS `claim` (
   `reg_date` date DEFAULT NULL,
   `modi_date` date DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8;
+);
 
-DROP TABLE IF EXISTS `company`;
-CREATE TABLE IF NOT EXISTS `company` (
+
+CREATE TABLE `company` (
   `id` bigint(20) NOT NULL AUTO_INCREMENT,
   `ceo_name` varchar(50) DEFAULT NULL,
   `biz_reg_num` varchar(50) DEFAULT NULL,
@@ -52,10 +50,10 @@ CREATE TABLE IF NOT EXISTS `company` (
   `zip_code` varchar(50) DEFAULT NULL,
   `establish_date` date DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
+);
 
-DROP TABLE IF EXISTS `located_pos`;
-CREATE TABLE IF NOT EXISTS `located_pos` (
+
+CREATE TABLE `located_pos` (
   `id` bigint(20) NOT NULL DEFAULT 0,
   `purchase_prop_id` bigint(20) DEFAULT NULL,
   `x_pos` double DEFAULT NULL,
@@ -63,10 +61,10 @@ CREATE TABLE IF NOT EXISTS `located_pos` (
   `z_pos` double DEFAULT NULL,
   `order` int(11) DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+);
 
-DROP TABLE IF EXISTS `login_history`;
-CREATE TABLE IF NOT EXISTS `login_history` (
+
+CREATE TABLE `login_history` (
   `id` bigint(20) NOT NULL AUTO_INCREMENT,
   `user_id` bigint(20) DEFAULT NULL,
   `login_date` date DEFAULT NULL,
@@ -74,10 +72,10 @@ CREATE TABLE IF NOT EXISTS `login_history` (
   `is_login` tinyint(4) DEFAULT NULL,
   `ip_checked` varchar(50) DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+);
 
-DROP TABLE IF EXISTS `owned_history`;
-CREATE TABLE IF NOT EXISTS `owned_history` (
+
+CREATE TABLE `owned_history` (
   `id` bigint(20) NOT NULL AUTO_INCREMENT,
   `reg_date` date DEFAULT NULL,
   `modi_date` date DEFAULT NULL,
@@ -85,10 +83,10 @@ CREATE TABLE IF NOT EXISTS `owned_history` (
   `user_id` bigint(20) DEFAULT NULL,
   `sale_prop_id` bigint(20) DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
+);
 
-DROP TABLE IF EXISTS `prop_price`;
-CREATE TABLE IF NOT EXISTS `prop_price` (
+
+CREATE TABLE `prop_price` (
   `id` bigint(20) NOT NULL AUTO_INCREMENT,
   `price` bigint(20) DEFAULT NULL,
   `reg_date` date DEFAULT NULL,
@@ -97,10 +95,10 @@ CREATE TABLE IF NOT EXISTS `prop_price` (
   `description` varchar(50) DEFAULT NULL,
   `purchase_prop_id` bigint(20) DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+);
 
-DROP TABLE IF EXISTS `purchase_prop`;
-CREATE TABLE IF NOT EXISTS `purchase_prop` (
+
+CREATE TABLE `purchase_prop` (
   `id` bigint(20) NOT NULL AUTO_INCREMENT,
   `associate_id` bigint(20) DEFAULT NULL,
   `city` varchar(50) DEFAULT NULL,
@@ -112,10 +110,10 @@ CREATE TABLE IF NOT EXISTS `purchase_prop` (
   `purchase_date` date DEFAULT NULL,
   `prop_type` varchar(50) DEFAULT 'REQUIRED',
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8;
+);
 
-DROP TABLE IF EXISTS `receipt`;
-CREATE TABLE IF NOT EXISTS `receipt` (
+
+CREATE TABLE `receipt` (
   `id` bigint(20) NOT NULL AUTO_INCREMENT,
   `claim_id` bigint(20) DEFAULT NULL,
   `payment` bigint(20) DEFAULT NULL,
@@ -124,10 +122,10 @@ CREATE TABLE IF NOT EXISTS `receipt` (
   `buyer_ip` varchar(50) DEFAULT NULL,
   `user_id` bigint(20) DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+);
 
-DROP TABLE IF EXISTS `sale_prop`;
-CREATE TABLE IF NOT EXISTS `sale_prop` (
+
+CREATE TABLE `sale_prop` (
   `id` bigint(20) NOT NULL AUTO_INCREMENT,
   `associate_id` bigint(20) DEFAULT NULL,
   `address_detail` varchar(50) DEFAULT NULL,
@@ -135,10 +133,10 @@ CREATE TABLE IF NOT EXISTS `sale_prop` (
   `reg_date` date DEFAULT NULL,
   `modi_date` date DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8;
+);
 
-DROP TABLE IF EXISTS `staff`;
-CREATE TABLE IF NOT EXISTS `staff` (
+
+CREATE TABLE `staff` (
   `id` bigint(20) NOT NULL AUTO_INCREMENT,
   `staff_role` varchar(50) DEFAULT 'STAFF',
   `reg_date` date DEFAULT NULL,
@@ -146,14 +144,14 @@ CREATE TABLE IF NOT EXISTS `staff` (
   `associate_id` bigint(20) DEFAULT NULL,
   `user_id` bigint(20) DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
+);
 
-DROP TABLE IF EXISTS `user`;
-CREATE TABLE IF NOT EXISTS `user` (
+
+CREATE TABLE `user` (
   `id` bigint(20) NOT NULL AUTO_INCREMENT,
   `username` varchar(50) NOT NULL,
   `company_id` bigint(20) DEFAULT NULL,
-  `password` varchar(70) NOT NULL,
+  `password` varchar(60) NOT NULL,
   `name` varchar(50) DEFAULT NULL,
   `email` varchar(50) DEFAULT NULL,
   `profile_Img` varchar(50) DEFAULT 'img/proflie_img.jpg',
@@ -165,4 +163,4 @@ CREATE TABLE IF NOT EXISTS `user` (
   `modi_password_date` date DEFAULT NULL,
   `phone_number` varchar(50) DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=utf8;
+);
