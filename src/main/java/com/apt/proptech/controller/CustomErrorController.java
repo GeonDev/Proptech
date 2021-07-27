cfa5abe03e934fc4faba41fe6912c01b5870204b
SonGeon <geons.dev@gmail.com>
2021-07-27 18:34:24 +0900

    커스텀 페이지 변경중

--- src/main/java/com/apt/proptech/controller/CustomErrorController.java
+++ src/main/java/com/apt/proptech/controller/CustomErrorController.java
@@ -15,11 +15,20 @@ public class CustomErrorController implements ErrorController {
     @GetMapping("/error")
     public String handleError(HttpServletRequest request, Model model) {
         Object status = request.getAttribute(RequestDispatcher.ERROR_STATUS_CODE);
+
+        model.addAttribute("contentName","");
+
         if (status != null) {
             int statusCode = Integer.valueOf(status.toString());
+
             if (statusCode == HttpStatus.NOT_FOUND.value()) {
-                return "errors/404-custom";
+                model.addAttribute("error404", true);
+
+            }else if(statusCode == HttpStatus.INTERNAL_SERVER_ERROR.value()){
+                model.addAttribute("error500", true);
             }
+
+            return "main";
         }
         return "error";
     }
