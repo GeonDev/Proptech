9f2b006cc8e82875c93bac83ebda404753f0bcaa
SonGeon <geons.dev@gmail.com>
2021-09-12 15:41:52 +0900

    모듈 제거

--- src/main/java/com/apt/proptech/ProptechApplication.java
+++ src/main/java/com/apt/proptech/ProptechApplication.java
@@ -1,6 +1,5 @@
 package com.apt.proptech;
 
-import com.ulisesbocchio.jasyptspringboot.annotation.EnableEncryptableProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
